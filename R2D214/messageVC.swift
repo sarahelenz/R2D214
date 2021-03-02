01d00c635e12ac20f9e0725a1892a78360975194
NikoHatzisvas <nikohatzisavas@outlook.com>
2021-03-02 15:21:57 -0600

    initial storyboard complete

---
+++ R2D214/messageVC.swift
@@ -0,0 +1,21 @@
+//
+//  messageVC.swift
+//  R2D214
+//
+//  Created by Niko on 3/2/21.
+//  Copyright © 2021 user178354. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class messageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
