3457b84402377c7f8f4f1b737102be97c73c6679
Sarah Elenz <selenz3112@stu.d214.org>
2021-03-04 15:36:11 -0500

    Renamed view controllers and added tableview protocol stubs

---
+++ R2D214/ClassTableView.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  R2D214
+//
+//  Created by user178354 on 2/25/21.
+//  Copyright © 2021 user178354. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDataSource {
+   
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 5
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           <#code#>
+       }
+}
+
