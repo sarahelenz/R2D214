c537a03afb70a0b0467428ac9fb8fec68d613cc4
NikoHatzisvas <nikohatzisavas@outlook.com>
2021-03-04 14:59:36 -0600

    alert code added

--- R2D214/FirstViewController.swift
+++ R2D214/FirstViewController.swift
@@ -8,22 +8,33 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UITableViewDataSource {
-   
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let messageAlert = UIAlertController(title: "", message: "Is this the group you would like to send a message to?", preferredStyle: .alert)
     @IBOutlet weak var tableView1: UITableView!    
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let yesAction = UIAlertAction(title: "Yes", style: .default) { [unowned messageAlert] _ in
+            let messageVCC = messageVC(nibName: "messageVC", bundle: nil)
+            self.navigationController?.pushViewController(messageVCC, animated: true)
+        }
+        let noAction = UIAlertAction(title: "No", style: .default) { [unowned messageAlert] _ in
+            return
+        }
+        messageAlert.addAction(yesAction)
+        messageAlert.addAction(noAction)
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return 5
-       }
-       
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
         cell.textLabel?.text = ""
         return cell
-       }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        present(messageAlert, animated: true, completion: nil)
+    }
 }
