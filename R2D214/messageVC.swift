3c1c580ce07b718cb6fe08e51ca087153058e2c3
NikoHatzisvas <nikohatzisavas@outlook.com>
2021-03-04 15:09:34 -0600

    send message code added, will be finished once firebase added

--- R2D214/messageVC.swift
+++ R2D214/messageVC.swift
@@ -8,14 +8,30 @@
 
 import Foundation
 import UIKit
+import MessageUI
+class messageVC: UIViewController, MFMailComposeViewControllerDelegate {
 
-class messageVC: UIViewController {
-
+    @IBOutlet weak var field: UITextField!
+    @IBOutlet weak var sendButtnon: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients([""])//will need to complete line with email address from firebase once available
+            mail.setMessageBody("<p>\(field.text!)</p>", isHTML: true)
+            present(mail, animated: true)
+        } else {
+            // show failure alert
+        }
+    }
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+    @IBAction func sendAct(_ sender: Any) {
+        sendEmail()//needs testing once emails added from firebase
+    }    
 }
-
