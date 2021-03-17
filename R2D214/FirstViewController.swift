dad4bae0b2bf965ba27f21ae832d7fdf236329f4
Sarah Elenz <selenz3112@stu.d214.org>
2021-03-16 21:42:58 -0400

    Created extension to access array letters/digits

--- R2D214/FirstViewController.swift
+++ R2D214/FirstViewController.swift
@@ -15,6 +15,8 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
     let messageAlert = UIAlertController(title: "", message: "Is this the group you would like to send a message to?", preferredStyle: .alert)
     @IBOutlet weak var tableView1: UITableView!
     var tableViewCount = [1,2,3,4,5]
+    var idnum: [String] = []
+    var arrayCount = 0
     
     override func viewDidLoad() {
         tableView1.dataSource = self
@@ -54,28 +56,32 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
             let EmailDictionary = dictionary["E-mail"] as! String
             let firstNameDictionary = dictionary["First Name"] as! String
             let lastNameDictionary = dictionary["Last Name"] as! String
-
+                
+                self.idnum.append(IDNumber)
             }
             self.tableView1.reloadData()
         }
     }
+    }
+    
+    func getYearNumbers(){
+        for ids in idnum{
+            var id = idnum[arrayCount]
+            var yearNumber = id[1]
+            arrayCount += 1
+        }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableViewCount.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let classYears = arrayOf.IDNumber[1...2]
+       
         var yearNumbers: [Int] = []
-        for year in classYears{
-            yearNumbers.append(year)
-            if classYears.contains(year){
-                break
-            }
-        }
-        let classTitles = ["Class of \(yearNumbers[0])", "Class of \(yearNumbers[1])", "Class of \(yearNumbers[2])", "Class of \(yearNumbers[3])", "Entire School"]
+        
+       // let classTitles = ["Class of \(yearNumbers[0])", "Class of \(yearNumbers[1])", "Class of \(yearNumbers[2])", "Class of \(yearNumbers[3])", "Entire School"]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
-        cell.textLabel?.text = "\(classTitles[indexPath.row])"
+   //     cell.textLabel?.text = "\(classTitles[indexPath.row])"
         
         return cell
     
