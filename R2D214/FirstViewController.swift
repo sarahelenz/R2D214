61454df838f49c617c391ce5a9094edd6722f4c8
Alex.2037 <aboguszewski2037@stu.d214.org>
2021-03-18 11:01:08 -0400

    added for loop

--- R2D214/FirstViewController.swift
+++ R2D214/FirstViewController.swift
@@ -17,6 +17,7 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
     var tableViewCount = [1,2,3,4,5]
     
     override func viewDidLoad() {
+        
         tableView1.dataSource = self
         super.viewDidLoad()
         let yesAction = UIAlertAction(title: "Yes", style: .default) { [unowned messageAlert] _ in
@@ -30,62 +31,34 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
         messageAlert.addAction(yesAction)
         messageAlert.addAction(noAction)
     }
-    public func getData()
-    {
-        arrayOf.IDNumber = []
-        arrayOf.counselor = []
-        arrayOf.Email = []
-        arrayOf.firstName = []
-        arrayOf.lastName = []
-        
-        let reference = Database.database().reference()
-         reference.observeSingleEvent(of: .value) { (snapshot) in
-        let students : [String:Any] = ["First Name" : "", "Last Name" : "", "Counselor" : "", "Email" : ""]
-        reference.child("r2d214-a33ff-default-rtdb").childByAutoId().setValue(students)
-        reference.observeSingleEvent(of: .value) { (snapshot) in
-            //     print (snapshot)
-            for data in snapshot.children.allObjects as! [DataSnapshot] {
-
-
-                let IDNumber = data.key
-
-            let dictionary = data.value as! NSDictionary
-            let CounselorDictionary = dictionary["counselor"] as! String
-            let EmailDictionary = dictionary["E-mail"] as! String
-            let firstNameDictionary = dictionary["First Name"] as! String
-            let lastNameDictionary = dictionary["Last Name"] as! String
-
-            }
-            self.tableView1.reloadData()
-        }
-    }
-    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableViewCount.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let classYears = arrayOf.IDNumber[1...2]
-        var yearNumbers: [Int] = []
-        for year in classYears{
-            yearNumbers.append(year)
-            if classYears.contains(year){
-                break
-            }
-        }
-        let classTitles = ["Class of \(yearNumbers[0])", "Class of \(yearNumbers[1])", "Class of \(yearNumbers[2])", "Class of \(yearNumbers[3])", "Entire School"]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
-        cell.textLabel?.text = "\(classTitles[indexPath.row])"
+//        let classYears = arrayOf.IDNumber[0]
+//        print(arrayOf.IDNumber)
+//        var yearNumbers: [Int] = []
+//        for year in classYears{
+//            yearNumbers.append(year)
+//            if classYears.contains(year){
+//                break
+//            }
+//        }
+//        let classTitles = ["Class of \(yearNumbers[0])", "Class of \(yearNumbers[1])", "Class of \(yearNumbers[2])", "Class of \(yearNumbers[3])", "Entire School"]
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+//        cell.textLabel?.text = "\(classTitles[indexPath.row])"
+//
+        return UITableViewCell()
         
-        return cell
-    
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         present(messageAlert, animated: true, completion: nil)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       let nvc = segue.destination as! ArrayOf
+        let nvc = segue.destination as! ArrayOf
         
     }
 }
